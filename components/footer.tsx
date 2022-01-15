3d845856c8bb729938569a9163fbf33020011e00
OREKI11 <93574822+OREKI11@users.noreply.github.com>
2022-01-15 18:51:08 +0530

    Create footer.tsx
    footer footer

---
+++ components/footer.tsx
@@ -0,0 +1,57 @@
+    <footer class="text-gray-600 body-font" style="background-color: rgb(2, 2, 32);">
+        <div class="container px-2 py-2  flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+                <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-300">
+
+                    </svg>
+                    <span class="ml-3 text-xl">  Throwitin</span>
+                </a>
+                <p class="mt-2 text-sm text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta similique architecto ipsum aut, cupiditate sit quia dolores sint hic quod minima rerum laudantium cumque consectetur, impedit deleniti dolorem ratione possimus!</p>
+
+                <div class="bg-white-100">
+                    <div class="container mx-auto py-6 px-10 flex flex-wrap flex-col sm:flex-row">
+
+                        <!-- <span class="inline-flex sm:ml-auto sm:mt-0 mt-8 justify-center sm:justify-start">
+                   -->
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                        </a>
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                        </a>
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                      <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                      <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                    </svg>
+                        </a>
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="flex-grow flex flex-wrap md:pl-40 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                <div class="lg:w-1/2 md:w-1/2 w-full px-6">
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">Start Project</h2>
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">Fund Project</h2>
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">About us</h2>
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">Contact us</h2>
+                </div>
+                <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">Privacy Policy</h2>
+                    <h2 class="title-font font-medium text-gray-300 tracking-widest text-sm mb-3">terms and conditions</h2>
+                </div>
+
+            </div>
+
+    </footer>
+
+
+</body>
+
+</html>
