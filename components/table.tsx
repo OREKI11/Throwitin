58449fb3a32e90e1e92f47b87ab1a778b8a19b18
OREKI11 <93574822+OREKI11@users.noreply.github.com>
2022-01-15 18:54:41 +0530

    Create table.tsx
    table

---
+++ components/table.tsx
@@ -0,0 +1,227 @@
+import * as React from "react"
+
+type Props = {
+    css: string
+}
+
+const Button: React.FC<Props> = ({  css }) => {
+    return (
+       
+
+    <h1 style="text-align: center; "> TOP projects</h1>
+    <div className="container flex justify-center mx-auto  ">
+        <div>
+            <div className="w-full ">
+                <div>
+                    <table className=" divide-gray-300 ">
+                        <thead className="bg-gray-50">
+                            <tr style="background-color: rgb(140, 146, 172); ">
+                                <th className="px-6 py-3  text-xs text-white-500">
+                                    Rank
+                                </th>
+                                <th className="px-6 py-3 text-xs text-white-500">
+                                    Project name
+                                </th>
+
+                                <th className="px-6 py-3text-xs text--500">
+                                    Fund raised
+                                </th>
+
+                            </tr>
+                        </thead>
+                        <tbody style="background-color:rgb(2, 2, 32) ;">
+                            <tr className="whitespace-nowrap ">
+
+                                <td className="px-6 py-4 ">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br> <br>
+
+    <h1 style="text-align: center;"> TOP Investors</h1>
+    <div className="container flex justify-center mx-auto  ">
+        <div>
+            <div className="w-full ">
+                <div>
+                    <table className=" divide-gray-300 ">
+                        <thead className="bg-gray-50">
+                            <tr style="background-color: rgb(140, 146, 172); ">
+                                <th className="px-6 py-3  text-xs text-white-500">
+                                    Rank
+                                </th>
+                                <th className="px-6 py-3 text-xs text-white-500">
+                                    Project name
+                                </th>
+
+                                <th className="px-6 py-3text-xs text--500">
+                                    Fund raised
+                                </th>
+
+                            </tr>
+                        </thead>
+                        <tbody style="background-color:rgb(2, 2, 32) ;">
+                            <tr className="whitespace-nowrap ">
+
+                                <td className="px-6 py-4 ">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+                            </tr>
+                            <tr className="whitespace-nowrap">
+
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-900">
+                                        1
+                                    </div>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <div className="text-sm text-white-500">Project number 1</div>
+                                </td>
+                                <td className="px-6 py-4 text-sm text-white-500">
+                                    60kUSDC
+                                </td>
+
+
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
